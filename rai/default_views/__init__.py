375d31d43ba90a217d80b2bf781b80918691d996
patta42 <patrick.happel@rub.de>
2020-03-21 01:21:25 +0100

    update to import the default views

---
+++ rai/default_views/__init__.py
@@ -0,0 +1,7 @@
+from .list import ListView
+from .detail import DetailView
+from .edit import EditView
+from .delete import DeleteView
+from .inactivate import InactivateView
+from .history import HistoryView
+from .create import CreateView
