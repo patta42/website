e0ce54e2d7e664575d5d66c2090d45d5851d8d7e
patta42 <patrick.happel@rub.de>
2020-03-23 23:31:38 +0100

    Use clone_kwargs instead of clone

--- rai/edit_handlers/edit_handlers.py
+++ rai/edit_handlers/edit_handlers.py
@@ -1,6 +1,10 @@
-from .generic import RAIBaseFormEditHandler
-from wagtail.admin.edit_handlers import FieldPanel
+from .generic import RAIBaseFormEditHandler, RAIBaseCompositeEditHandler
 
+from django.core.exceptions import ImproperlyConfigured
+
+import random
+import string
+import uuid
 
 class RAITabbedInterface(RAIBaseFormEditHandler):
     template = "wagtailadmin/edit_handlers/tabbed_interface.html"
@@ -17,19 +21,70 @@ class RAITabbedInterface(RAIBaseFormEditHandler):
         new.base_form_class = self.base_form_class
         return new
     
-class RAIObjectList(RAITabbedInterface):
+class RAIObjectList(RAIBaseFormEditHandler):
     template = 'rai/edit_handlers/object-list.html'
-
+    def __init__(self, *args, **kwargs):
+        self.base_form_class = kwargs.pop('base_form_class', None)
+        super().__init__(*args, **kwargs)
+    def clone_kwargs(self):
+        kwargs = super().clone_kwargs()
+        kwargs.update({'base_form_class': self.base_form_class})
+        return kwargs
+    
+    
 class RAIFieldRowPanel(RAIBaseFormEditHandler):
     template = "rai/edit_handlers/field-row-panel.html"
 
-    # def on_instance_bound(self):
-    #     super().on_instance_bound()
-
-    #     col_count = ' col%s' % (12 // len(self.children))
-    #     # If child panel doesn't have a col# class then append default based on
-    #     # number of columns
-    #     for child in self.children:
-    #         if not re.search(r'\bcol\d+\b', child.classname):
-    #             child.classname += col_count
+class RAICollapsablePanel(RAIBaseFormEditHandler):
+    is_collapsable = True
+    template = 'rai/edit_handlers/collapsable-panel.html'
     
+    def __init__(self, *args, **kwargs):
+        self.is_collapsed = kwargs.pop('is_collapsed', True)
+        self.collapse_id = kwargs.pop('collapse_id', uuid.uuid4())
+        self.is_expanded = kwargs.pop('is_expanded', False)
+        
+
+        heading = kwargs.pop('heading', None)
+        if heading is None:
+            raise ImproperlyConfigured(
+                "The attribute `heading` is required for {0}".format(
+                    self.__class__.__name__
+                )
+            )            
+        super().__init__( heading = heading, *args, **kwargs)
+
+    def clone_kwargs(self):
+        kwargs = super().clone_kwargs()
+        kwargs.update({
+            'collapse_id' : self.collapse_id,
+            'is_expanded' : self.is_expanded
+        })
+        return kwargs
+        
+class RAIUserDataPanel(RAICollapsablePanel):
+    """
+    A panel that shows information that is entered by external users.
+    Issues a visual warning to avoid editing
+    """
+    template = 'rai/edit_handlers/userdata-panel.html'
+class RAICollectionPanel(RAIBaseFormEditHandler):
+    template = 'rai/edit_handlers/collection-panel.html'
+    
+class RAIPillsPanel(RAIBaseFormEditHandler):
+    template = 'rai/edit_handlers/pills-panel.html'
+
+    def __init__(self, *args, **kwargs):
+        self.unique_id = kwargs.pop(
+            'unique_id',
+            ''.join(
+                random.choice(string.ascii_uppercase + string.ascii_lowercase) for _ in range(10)
+            )
+        )
+        super().__init__(*args, **kwargs)
+    def clone_kwargs(self):
+        kwargs = super().clone_kwargs()
+        kwargs.update({
+            'unique_id' : self.unique_id
+        })
+        return kwargs
