6f7db3bdb8b648b41e233838d367a713ae315a51
patta42 <patrick.happel@rub.de>
2020-03-24 00:52:30 +0100

    Example of edit action with edit_handler

--- userinput/rai/actions.py
+++ userinput/rai/actions.py
@@ -17,7 +17,11 @@ class RAIProjectListAction(ListAction):
     list_filters = [
         filters.ProjectStatusFilter
     ]
-
+class RAIProjectAction(EditAction):
+    edit_handler = eh.RAIObjectList([
+        
+    ])
+    
 class RAIWorkgroupListAction(ListAction):
     list_item_template = 'userinput/workgroup/rai/list/item-in-list.html'
     list_filters = [
@@ -26,12 +30,22 @@ class RAIWorkgroupListAction(ListAction):
 
 
 class RAIWorkgroupEditAction(EditAction):
-    edit_handler = eh.RAIObjectList(
-        [
-            eh.RAIFieldRowPanel([
-                eh.RAIFieldPanel('department_de'),
-                eh.RAIFieldPanel('department_en'),
-            ]),
-#            eh.RAIFieldPanel('title'),
-        ]
-    )
+    edit_handler = eh.RAIObjectList([
+        eh.RAIUserDataPanel([
+            eh.RAIPillsPanel([
+                eh.RAICollectionPanel([
+                    eh.RAIFieldPanel('department_de'),
+                    eh.RAIFieldPanel('institute_de'),
+                ], heading = 'deutsch'),
+                eh.RAICollectionPanel([
+                    eh.RAIFieldPanel('department_en'),
+                    eh.RAIFieldPanel('institute_en'),
+                ], heading = 'english'),
+            ], nav_label = 'Select language'),
+            eh.RAIFieldPanel('homepage')
+        ], heading = 'User data', is_expanded = False),
+        eh.RAICollapsablePanel([
+            eh.RAIFieldPanel('internal_rubion_comment')
+        ], heading = 'Internal comment', is_expanded = True)
+    ])
+
