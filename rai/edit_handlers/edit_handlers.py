7aac3a080dd244dd1edfb7dea6f8e50728ddf854
patta42 <patrick.happel@rub.de>
2020-03-24 14:52:00 +0100

    Added RAIMultiFieldPanel

--- rai/edit_handlers/edit_handlers.py
+++ rai/edit_handlers/edit_handlers.py
@@ -1,10 +1,44 @@
-from .generic import RAIBaseFormEditHandler, RAIBaseCompositeEditHandler, RAIFieldPanel
+from .generic import (
+    RAIEditHandler,
+    RAIBaseFormEditHandler,
+    RAIBaseCompositeEditHandler,
+    RAIFieldPanel
+)
 
 from django.core.exceptions import ImproperlyConfigured
+from django.forms.models import fields_for_model
+from django.template.loader import render_to_string
 
+from rai.forms import formfield_for_dbfield
 import random
 import string
 import uuid
+
+
+
+
+def extract_panel_definitions_from_model_class(model, exclude=None):
+    if hasattr(model, 'panels'):
+        return model.panels
+
+    panels = []
+
+    _exclude = []
+    if exclude:
+        _exclude.extend(exclude)
+
+    fields = fields_for_model(model, exclude=_exclude, formfield_callback=formfield_for_dbfield)
+
+    for field_name, field in fields.items():
+        try:
+            panel_class = field.widget.get_panel()
+        except AttributeError:
+            panel_class = RAIFieldPanel
+
+        panel = panel_class(field_name)
+        panels.append(panel)
+
+    return panels
 
 class RAITabbedInterface(RAIBaseFormEditHandler):
     template = "wagtailadmin/edit_handlers/tabbed_interface.html"
@@ -132,3 +166,146 @@ class RAITranslatedContentPanel(RAIPillsPanel):
 
     def clone(self):
         return RAIPillsPanel(**self.clone_kwargs())
+
+class RAIMultiFieldPanel(RAIBaseCompositeEditHandler):
+    template = 'rai/edit_handlers/multi-field-panel.html'
+
+    def classes(self):
+        classes = super().classes()
+        classes.append('multi-field')
+        return classes
+
+class RAIInlinePanel(RAIEditHandler):
+    def __init__(self, relation_name, panels=None, heading='', label='',
+                 min_num=None, max_num=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.relation_name = relation_name
+        self.panels = panels
+        self.heading = heading or label
+        self.label = label
+        self.min_num = min_num
+        self.max_num = max_num
+
+    def clone_kwargs(self):
+        kwargs = super().clone_kwargs()
+        kwargs.update(
+            relation_name=self.relation_name,
+            panels=self.panels,
+            label=self.label,
+            min_num=self.min_num,
+            max_num=self.max_num,
+        )
+        return kwargs
+
+    def get_panel_definitions(self):
+        # Look for a panels definition in the InlinePanel declaration
+        if self.panels is not None:
+            return self.panels
+        # Failing that, get it from the model
+        return extract_panel_definitions_from_model_class(
+            self.db_field.related_model,
+            exclude=[self.db_field.field.name]
+        )
+
+    def get_child_edit_handler(self):
+        panels = self.get_panel_definitions()
+        child_edit_handler = RAICollectionPanel(panels, heading=self.heading)
+        return child_edit_handler.bind_to(model=self.db_field.related_model)
+
+    def required_formsets(self):
+        child_edit_handler = self.get_child_edit_handler()
+        return {
+            self.relation_name: {
+                'fields': child_edit_handler.required_fields(),
+                'widgets': child_edit_handler.widget_overrides(),
+                'min_num': self.min_num,
+                'validate_min': self.min_num is not None,
+                'max_num': self.max_num,
+                'validate_max': self.max_num is not None
+            }
+        }
+
+    def html_declarations(self):
+        return self.get_child_edit_handler().html_declarations()
+
+    def get_comparison(self):
+        field_comparisons = []
+
+        for panel in self.get_panel_definitions():
+            field_comparisons.extend(
+                panel.bind_to(model=self.db_field.related_model)
+                .get_comparison())
+
+        return [functools.partial(compare.ChildRelationComparison, self.db_field, field_comparisons)]
+
+    def on_model_bound(self):
+        manager = getattr(self.model, self.relation_name)
+        self.db_field = manager.rel
+
+    def on_form_bound(self):
+        self.formset = self.form.formsets[self.relation_name]
+
+        self.children = []
+        for subform in self.formset.forms:
+            # override the DELETE field to have a hidden input
+            subform.fields[DELETION_FIELD_NAME].widget = forms.HiddenInput()
+
+            # ditto for the ORDER field, if present
+            if self.formset.can_order:
+                subform.fields[ORDERING_FIELD_NAME].widget = forms.HiddenInput()
+
+            child_edit_handler = self.get_child_edit_handler()
+            self.children.append(child_edit_handler.bind_to(
+                instance=subform.instance, request=self.request, form=subform))
+
+        # if this formset is valid, it may have been re-ordered; respect that
+        # in case the parent form errored and we need to re-render
+        if self.formset.can_order and self.formset.is_valid():
+            self.children.sort(
+                key=lambda child: child.form.cleaned_data[ORDERING_FIELD_NAME] or 1)
+
+        empty_form = self.formset.empty_form
+        empty_form.fields[DELETION_FIELD_NAME].widget = forms.HiddenInput()
+        if self.formset.can_order:
+            empty_form.fields[ORDERING_FIELD_NAME].widget = forms.HiddenInput()
+
+        self.empty_child = self.get_child_edit_handler()
+        self.empty_child = self.empty_child.bind_to(
+            instance=empty_form.instance, request=self.request, form=empty_form)
+
+    template = "wagtailadmin/edit_handlers/inline_panel.html"
+
+    def render(self):
+        formset = render_to_string(self.template, {
+            'self': self,
+            'can_order': self.formset.can_order,
+        })
+        js = self.render_js_init()
+        return widget_with_script(formset, js)
+
+    js_template = "wagtailadmin/edit_handlers/inline_panel.js"
+
+    def render_js_init(self):
+        return mark_safe(render_to_string(self.js_template, {
+            'self': self,
+            'can_order': self.formset.can_order,
+        }))
+
+
+class RAIMissingPanel(RAIEditHandler):
+    """
+    A panel indicating a missing Panel.
+    For conversion from Wagtail edit_handlers
+    """
+    template = "rai/edit_handlers/missing-panel.html"
+    def __init__(self, missing = None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.missing = missing
+    def clone_kwargs(self):
+        kwargs = super().clone_kwargs()
+        kwargs.update({'missing':self.missing})
+        return kwargs
+    def widget_overrides(self):
+        return {}
+    def render(self):
+        return render_to_string(self.template, {'self':self})
