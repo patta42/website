d63a6403c1a56ab7c92a1030a1a3970c86815535
patta42 <patrick.happel@rub.de>
2020-03-24 14:51:41 +0100

    use new RAIMultiFieldPanel

--- userinput/rai/actions.py
+++ userinput/rai/actions.py
@@ -1,5 +1,5 @@
 
-from rai.actions import ListAction, EditAction
+from rai.actions import ListAction, EditAction, DetailAction, CreateAction
 import rai.edit_handlers as eh
 from rai.widgets import RAITextInput
 import userinput.rai.filters as filters
@@ -14,12 +14,42 @@ class RAIUserDataListAction(ListAction):
     ]
 
 class RAIProjectListAction(ListAction):
+    list_item_template = 'userinput/project/rai/list/item-in-list.html'
     list_filters = [
         filters.ProjectStatusFilter
     ]
-class RAIProjectAction(EditAction):
+class RAIProjectEditAction(EditAction):
     edit_handler = eh.RAIObjectList([
-        
+        eh.RAIUserDataPanel([
+            eh.RAITranslatedContentPanel(
+                {'de':'german', 'en':'english'},
+                ['title', 'summary']
+            ),
+        ], heading = 'Project information', is_expanded = False),
+        eh.RAIUserDataPanel([
+            eh.RAIFieldPanel('is_confidential'),
+        ], heading = 'Project visibility', is_expanded = False),
+        eh.RAIUserDataPanel([
+            eh.RAIMultiFieldPanel([
+                eh.RAIFieldPanel('uses_gmos', classname="col-md-3"),
+                eh.RAIFieldPanel('gmo_info', classname="col-md-9"),
+            ], heading="Info on GMOs", classname='form-row'),
+            eh.RAIMultiFieldPanel([
+                eh.RAIFieldPanel('uses_chemicals', classname="col-md-3"),
+                eh.RAIFieldPanel('chemicals_info', classname="col-md-9"),
+            ],  heading="Info on chemicals", classname='form-row'),
+            eh.RAIMultiFieldPanel([
+                eh.RAIFieldPanel('uses_hazardous_substances', classname="col-md-3"),
+                eh.RAIFieldPanel('hazardous_info', classname="col-md-9"),
+            ], heading="Info on hazardous substance", classname='form-row'),
+            eh.RAIMultiFieldPanel([
+                eh.RAIFieldPanel('biological_agents', classname="col-md-3"),
+                eh.RAIFieldPanel('bio_info', classname="col-md-9"),
+            ], heading="Info on biological substances", classname='form-row')
+        ], heading = 'Safety information', is_expanded = True),
+        eh.RAICollapsablePanel([
+            eh.RAIFieldPanel('internal_rubion_comment')
+        ], heading = 'Internal remarks')
     ])
     
 class RAIWorkgroupListAction(ListAction):
@@ -28,18 +58,25 @@ class RAIWorkgroupListAction(ListAction):
         filters.WorkgroupStatusFilter
     ]
 
+workgroup_edit_handler = eh.RAIObjectList([
+    eh.RAIUserDataPanel([
+        eh.RAITranslatedContentPanel(
+            {'de':'german', 'en':'english'},
+            ['title', 'department', 'institute']
+        ),
+        eh.RAIFieldPanel('homepage')
+    ], heading = 'User data', is_expanded = False),
+    eh.RAICollapsablePanel([
+        eh.RAIFieldPanel('internal_rubion_comment')
+    ], heading = 'Internal comment', is_expanded = True)
+])
+
 
 class RAIWorkgroupEditAction(EditAction):
-    edit_handler = eh.RAIObjectList([
-        eh.RAIUserDataPanel([
-            eh.RAITranslatedContentPanel(
-                {'de':'german', 'en':'english'},
-                ['department', 'institute']
-            ),
-            eh.RAIFieldPanel('homepage')
-        ], heading = 'User data', is_expanded = False),
-        eh.RAICollapsablePanel([
-            eh.RAIFieldPanel('internal_rubion_comment')
-        ], heading = 'Internal comment', is_expanded = True)
-    ])
+    edit_handler = workgroup_edit_handler
 
+class RAIWorkgroupDetailAction(DetailAction):
+    edit_handler = workgroup_edit_handler
+
+class RAIWorkgroupCreateAction(CreateAction):
+    edit_handler = workgroup_edit_handler
