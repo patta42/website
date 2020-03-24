162e87735f6a553e754077d4ca1c893413471a72
patta42 <patrick.happel@rub.de>
2020-03-24 14:52:14 +0100

    initial commit

---
+++ rai/edit_handlers/__init__.py
@@ -0,0 +1,7 @@
+from .edit_handlers import (
+    RAIObjectList, RAIFieldRowPanel, RAIUserDataPanel,
+    RAIPillsPanel, RAICollectionPanel, RAICollapsablePanel,
+    RAITranslatedContentPanel, RAIInlinePanel, RAIMissingPanel,
+    RAIMultiFieldPanel
+)
+from .generic import RAIFieldPanel
