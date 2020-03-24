859802fe940d3ede3406f615206a6dde1423e1be
patta42 <patrick.happel@rub.de>
2020-03-24 13:58:34 +0100

    DetailView derived from EditView with edit_handler disabled

--- rai/default_views/detail.py
+++ rai/default_views/detail.py
@@ -1,4 +1,9 @@
-from .generic import RAIView
+from .edit import EditView
 
-class DetailView(RAIView):
-    pass
+class DetailView(EditView):
+    http_method_names = ['get']
+    
+    def get(self, request, **kwargs):
+        self.edit_handler = self.edit_handler.disable()
+        return super().get(request, **kwargs)
+
