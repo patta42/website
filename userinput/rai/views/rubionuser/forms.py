ec68bfc23f2b2a21fdce8e8dfafef0d383d039b7
patta42 <patrick.happel@rub.de>
2020-04-06 05:14:52 +0200

    Approach using a fieldsets setting
    Not fully working and no good approach. Anyway, commited to be able to role back to this.

--- userinput/rai/views/rubionuser/forms.py
+++ userinput/rai/views/rubionuser/forms.py
@@ -4,9 +4,15 @@ from django import forms
 from django.forms.formsets import BaseFormSet
 from django.forms.widgets import HiddenInput
 
-from rai.widgets import RAIRadioSelect, RAISelect,RAIRadioSelectTable
 
-class UserAndStaffInactivationForm(forms.Form):
+from rai.forms import RAIForm
+from rai.edit_handlers import RAIFieldPanel, RAIObjectList
+from rai.widgets import (
+    RAIRadioSelect, RAISelect, RAIRadioSelectTable,
+    RAICheckboxInput
+)
+
+class UserAndStaffInactivationForm(RAIForm):
     user_staff_choice = forms.ChoiceField(
         choices =(
             ('user_only', 'Nur den Nutzer inaktivieren'),
@@ -16,31 +22,67 @@ class UserAndStaffInactivationForm(forms.Form):
         widget = RAIRadioSelect,
         
     )
+    edit_handler = RAIFieldPanel('user_staff_choice')
 
 
-class WorkgroupInactivationForm(forms.Form):
+    
+class WorkgroupInactivationForm(RAIForm):
+    fieldsets = [
+        {
+            'id' : 'workgroup_choice_fieldset',
+            'legend' : 'Der Nutzer ist Leiter einer Arbeitsgruppe',
+            'help_path':'userinput:rubionuser:inactivate:is_group_leader',
+            'fields' : ['workgroup_choice'],
+            'intro' : 'Bitte entscheide, was mit der Gruppe <strong>»{{workgroup}}«</strong> und ggf. ihren Mitgliedern und Ihren Projekten geschehen soll.',
+            'fieldsets' : [{
+                'id': 'new_leader',
+                'legend':'Wer soll neuer Leiter der Arbeitsgruppe werden?',
+                'fields':['new_leader'],
+                'depends_on_field':'workgroup_choice',
+                'show_at_value':'new_leader' 
+            }]
+        }
+    ]
+
+    fieldsets = [
+        
+    ]
     workgroup_choice = forms.ChoiceField(
         choices = (
             ('inactivate', 'Die Gruppe inaktivieren'),
             ('new_leader', 'Anderen Nutzer zum AG-Leiter ernennen (andere Nutzer und Projekte werden beibehalten)'),
-        ),
+        ), 
         required = True,
         widget = RAIRadioSelect
     )
 
-    def __init__(self, *args, **kwargs):
-        members = kwargs.pop('members', None)
+    def __init__(self, members = None, projects = None, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['new_leader'] = forms.ChoiceField(
-            required = False,
-            choices = (
-                (member.pk, '{}, {}'.format(member.specific.last_name, member.specific.first_name))
-                for member in members),
-            widget = RAIRadioSelect
-        )
+        if members and members.count() > 0:
+            self.fields['new_leader'] = forms.ChoiceField(
+                required = False,
+                choices = (
+                    (member.pk, '{}, {}'.format(member.specific.last_name, member.specific.first_name))
+                    for member in members),
+                widget = RAIRadioSelect
+            )
+        else:
+            #self.fields.pop('workgroup_choice')
+            self.fields['workgroup_choice'] = forms.BooleanField(
+                disabled = True,
+                required = True,
+                label = 'Die Arbeitsgruppe hat keine weiteren Mitglieder und wird inaktiviert.',
+                widget = RAICheckboxInput(
+                    attrs = {
+                        'checked': True,
+                        'label': 'Die Arbeitsgruppe hat keine weiteren Mitglieder und wird inaktiviert.'
+                    }
+                ) ,
+                
+            )
         
 
-class MemberDecisionForm(forms.Form):
+class MemberDecisionForm(RAIForm):
     # fake field to get the user's name into the form
     name = forms.CharField(required = False)
     pk = forms.IntegerField(widget=HiddenInput)
