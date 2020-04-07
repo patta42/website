5add65b1782b255b7f973ab31a439d1b58e36981
patta42 <patrick.happel@rub.de>
2020-04-07 09:01:44 +0200

    adapted actions to new feature show_for_instance

--- userinput/rai/actions.py
+++ userinput/rai/actions.py
@@ -1,17 +1,62 @@
+from pprint import pprint 
 
-from rai.actions import ListAction, EditAction, DetailAction, CreateAction
+import datetime
+
+from rai.actions import (
+    ListAction, EditAction, DetailAction, CreateAction, InactivateAction, SpecificAction
+)
 import rai.edit_handlers as eh
-from rai.widgets import RAITextInput
-import userinput.rai.filters as filters
-    #RUBIONUserStatusFilter, RUBIONUserInstrumentFilter
+from rai.widgets import RAIRadioInput, RAISelectMultiple, RAISelect
 
-    
+import userinput.rai.filters as filters
+from userinput.rai.edit_handlers import (
+    project_edit_handler, workgroup_edit_handler, rubionuser_edit_handler
+)
+
+
+# an  action for moving from one group to another
+# used by projects and rubionuser
+class MoveToWorkgroupAction(SpecificAction):
+    label = 'Anderer Gruppe zuordnen'
+    icon = 'arrow-alt-circle-right'
+    icon_font = 'fas'
+    action_identifier = 'move'
+
+    def get_view(self):
+        return self.raiadmin.moveview.as_view(
+            raiadmin = self.raiadmin,
+            active_action = self
+        )
+
+
+def _is_rubion_user_active(instance):
+    return (
+        instance.linked_user is None or
+        instance.expire_at is None or
+        instance.expire_at >= datetime.datetime.now()
+    )
+class RUBIONUserOnlyWhenActiveMixin:
+    def show_for_instance(self, instance):
+        return _is_rubion_user_active(instance)
+
+class RUBIONUserMoveAction(RUBIONUserOnlyWhenActiveMixin, MoveToWorkgroupAction):
+    def show_for_instance(self, instance):
+        return super().show_for_instance(instance) and not instance.is_leader
+
+class RUBIONUserInactivateAction(RUBIONUserOnlyWhenActiveMixin, InactivateAction):
+    pass
+        
 class RAIUserDataListAction(ListAction):
     list_item_template = 'userinput/rubionuser/rai/list/item-in-list.html'
     list_filters = [
         filters.RUBIONUserStatusFilter,
         filters.RUBIONUserInstrumentFilter
     ]
+class RUBIONUserDataEditAction(RUBIONUserOnlyWhenActiveMixin, EditAction):
+    edit_handler = rubionuser_edit_handler
+
+
+
 
 class RAIProjectListAction(ListAction):
     list_item_template = 'userinput/project/rai/list/item-in-list.html'
@@ -19,38 +64,8 @@ class RAIProjectListAction(ListAction):
         filters.ProjectStatusFilter
     ]
 class RAIProjectEditAction(EditAction):
-    edit_handler = eh.RAIObjectList([
-        eh.RAIUserDataPanel([
-            eh.RAITranslatedContentPanel(
-                {'de':'german', 'en':'english'},
-                ['title', 'summary']
-            ),
-        ], heading = 'Project information', is_expanded = False),
-        eh.RAIUserDataPanel([
-            eh.RAIFieldPanel('is_confidential'),
-        ], heading = 'Project visibility', is_expanded = False),
-        eh.RAIUserDataPanel([
-            eh.RAIMultiFieldPanel([
-                eh.RAIFieldPanel('uses_gmos', classname="col-md-3"),
-                eh.RAIFieldPanel('gmo_info', classname="col-md-9"),
-            ], heading="Info on GMOs", classname='form-row'),
-            eh.RAIMultiFieldPanel([
-                eh.RAIFieldPanel('uses_chemicals', classname="col-md-3"),
-                eh.RAIFieldPanel('chemicals_info', classname="col-md-9"),
-            ],  heading="Info on chemicals", classname='form-row'),
-            eh.RAIMultiFieldPanel([
-                eh.RAIFieldPanel('uses_hazardous_substances', classname="col-md-3"),
-                eh.RAIFieldPanel('hazardous_info', classname="col-md-9"),
-            ], heading="Info on hazardous substance", classname='form-row'),
-            eh.RAIMultiFieldPanel([
-                eh.RAIFieldPanel('biological_agents', classname="col-md-3"),
-                eh.RAIFieldPanel('bio_info', classname="col-md-9"),
-            ], heading="Info on biological substances", classname='form-row')
-        ], heading = 'Safety information', is_expanded = True),
-        eh.RAICollapsablePanel([
-            eh.RAIFieldPanel('internal_rubion_comment')
-        ], heading = 'Internal remarks')
-    ])
+    edit_handler = project_edit_handler
+    
     
 class RAIWorkgroupListAction(ListAction):
     list_item_template = 'userinput/workgroup/rai/list/item-in-list.html'
@@ -58,19 +73,7 @@ class RAIWorkgroupListAction(ListAction):
         filters.WorkgroupStatusFilter
     ]
 
-workgroup_edit_handler = eh.RAIObjectList([
-    eh.RAIUserDataPanel([
-        eh.RAITranslatedContentPanel(
-            {'de':'german', 'en':'english'},
-            ['title', 'department', 'institute']
-        ),
-        eh.RAIFieldPanel('homepage')
-    ], heading = 'User data', is_expanded = False),
-    eh.RAICollapsablePanel([
-        eh.RAIFieldPanel('internal_rubion_comment')
-    ], heading = 'Internal comment', is_expanded = True)
-])
-
+    
 
 class RAIWorkgroupEditAction(EditAction):
     edit_handler = workgroup_edit_handler
@@ -80,3 +83,4 @@ class RAIWorkgroupDetailAction(DetailAction):
 
 class RAIWorkgroupCreateAction(CreateAction):
     edit_handler = workgroup_edit_handler
+    text_type = 'secondary'
