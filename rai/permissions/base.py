9dafa4504987417e3297991b2e65fb7102094ca8
patta42 <patrick.happel@rub.de>
2020-04-10 01:08:04 +0200

    moved PERMISSIONS and related to new file internals.py

--- rai/permissions/base.py
+++ rai/permissions/base.py
@@ -1,2 +1,21 @@
+import rai.permissions.permissions as perm
 
-REGISTERED_ITEMS = []
+from wagtail.core import hooks
+
+# this is how to register a single permission
+@hooks.register('register_rai_permission')
+def register_default_permission():
+    return ('__all__', '__all__', perm.NonePermission)
+
+# and this is how to register multiple permissions
+@hooks.register('register_rai_permissions')
+def register_default_permissions():
+    return ('__all__', '__all__', [
+        perm.ViewPermission,
+        perm.EditPermission,
+        perm.CreatePermission,
+        perm.DeletePermission
+    ])
+
+
+    
