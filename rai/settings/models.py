268211cab19b22ea5be874670f59004470145509
patta42 <patrick.happel@rub.de>
2020-04-15 09:06:41 +0200

    added helper methods for json and updated save
    The model fields now use JsonField, which is used in the save() method to
    check whether the corresponding value needs to be jsonified.
    
    as_dict():
      returns the values of the object as a python dict
    
    update(dictionary):
      updates the values of the object from a dictionary
    
    save():
      checks for required json and jsonifies the value if it is not a string

--- rai/settings/models.py
+++ rai/settings/models.py
@@ -1,6 +1,8 @@
+from .fields import JsonField
 from django.conf import settings
 from django.db import models
 
+import json
 
 class ListFilterSettings(models.Model):
     view_name = models.CharField(max_length = 128)
@@ -9,12 +11,41 @@ class ListFilterSettings(models.Model):
 
 class AdminMenuSettings(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
-    group_order = models.TextField(
+    group_order = JsonField(
         blank = True
     )
-    item_labels = models.TextField(
+    item_labels = JsonField(
         blank = True
     )
-    group_item_labels = models.TextField(
+    group_item_labels = JsonField(
         blank = True
     )
+
+    def save(self, *args, **kwargs):
+        # this jsonifies every JsonField
+        for field in self._meta.fields:
+            if isinstance(field, JsonField):
+                attr = getattr(self, field.name)
+                if attr and not isinstance(attr, str):
+                    setattr(self, field.name, json.dumps(attr))
+                    
+        super().save(*args, **kwargs)
+
+    def as_dict(self):
+        dct = {
+            'group_order' : [],
+            'item_labels' : {},
+            'group_item_labels' : {}
+        }
+        if self.group_order:
+            dct.update({'group_order' : json.loads(self.group_order)})
+        if self.item_labels:
+            dct.update({'item_labels' : json.loads(self.item_labels)})
+        if self.group_item_labels:
+            dct.update({'group_item_labels' : json.loads(self.group_item_labels)})
+
+        return dct
+    def update(self, dct):
+        for k,v in dct.items():
+            if v and hasattr(self, k):
+                setattr(self, k , v)
