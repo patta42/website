f6431a6bd860b413d8c58072a9f68003955968c3
patta42 <patrick.happel@rub.de>
2020-04-27 08:54:21 +0200

    added create action

--- userdata/rai/actions.py
+++ userdata/rai/actions.py
@@ -1,7 +1,13 @@
-
-from .filters import StaffUserStatusFilter 
-from rai.actions import ListAction
+from .filters import StaffUserStatusFilter
+from .edit_handlers import create_staff_edit_handler, edit_staff_user_handler
+from rai.actions import ListAction, CreateAction, EditAction
 
 class RAIStaffUserListAction(ListAction):
     list_filters = [ StaffUserStatusFilter ]
+    list_item_template = 'userdata/staffuser/rai/list/item-in-list.html'
     
+class StaffUserCreateAction(CreateAction):
+    edit_handler = create_staff_edit_handler
+
+class StaffUserEditAction(EditAction):
+    edit_handler = edit_staff_user_handler
