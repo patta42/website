8d33502abc729d49c600fd6d0434530bf39d04ae
patta42 <patrick.happel@rub.de>
2020-05-08 07:14:44 +0200

    added selectElementText

--- rubion/static/js/admin/RUBIONtail.js
+++ rubion/static/js/admin/RUBIONtail.js
@@ -357,6 +357,21 @@ $R.help = {
     })()
 }
 
+$R.selectElementText= function(el, win) {
+    win = win || window;
+    var doc = win.document, sel, range;
+    if (win.getSelection && doc.createRange) {
+        sel = win.getSelection();
+        range = doc.createRange();
+        range.selectNodeContents(el);
+        sel.removeAllRanges();
+        sel.addRange(range);
+    } else if (doc.body.createTextRange) {
+        range = doc.body.createTextRange();
+        range.moveToElementText(el);
+        range.select();
+    }
+}
 
 $(document).ready(
     function(){
