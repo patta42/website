0a5e12d4e9316bfeb3c0fbc514c1fbe1958791aa
patta42 <patrick.happel@rub.de>
2020-05-08 08:23:44 +0200

    made save_post_hook return if something was saved and adding messages optional

--- rai/default_views/edit.py
+++ rai/default_views/edit.py
@@ -6,6 +6,7 @@ from .create import CreateView
 
 
 from django.contrib import messages
+from django.core.exceptions import ValidationError
 from django.shortcuts import redirect
 from django.template.loader import render_to_string
 
@@ -15,7 +16,7 @@ from django.utils.translation import ugettext_lazy as _
 class EditView(CreateView, SingleObjectMixin):
     template_name = 'rai/views/default/edit.html'
     formclass = None
-    
+        
     def get_actions(self):
         return self.get_group_actions() + self.get_item_actions()
     
@@ -38,8 +39,13 @@ class EditView(CreateView, SingleObjectMixin):
         context['object'] = self.obj
         return context
 
-    def form_for_post(self, request):
-        form = self.formclass(request.POST, request.FILES, instance = self.obj, user = request.user)
+    def form_for_post_hook(self, request):
+        '''
+        Sets up the form and formsets for post.
+        returns the form and sets self.formset
+        '''
+        form = self.formclass(request.POST, request.FILES,
+                              instance = self.obj, user = request.user)
         self.formsets = {}
         for key, formset_class in self.formset_classes.items():
             self.formsets.update({
@@ -49,10 +55,36 @@ class EditView(CreateView, SingleObjectMixin):
                 )
             })
         return form
+
+    def save_post_hook(self, request, form, add_messages = True):
+        saved_something = False
+        if form.has_changed():
+            form.save()
+            saved_something = True
+                
+        for formset in self.formsets.values():
+            for subform in formset.forms:
+                if subform.has_changed():
+                    subform.save()
+                    saved_something = True
+        if add_messages:
+            if saved_something:
+                messages.success(request, _('The data was updated.'))
+            else:
+                messages.info(request, _('The data contained not changes and therefore was not updated.'))
+        return saved_something
     
     def post(self, request, *args, **kwargs):
-        self.edit_handler = self.edit_handler.bind_to(instance = self.obj)        
-        form = self.form_for_post(request)
+        '''
+        This default post method calls two hooks:
+        
+        form_for_post_hook:
+          setup the forms required for this post
+        save_post_hook:
+           called when everythin is valid
+        '''
+        self.edit_handler = self.edit_handler.bind_to(instance = self.obj)
+        form = self.form_for_post_hook(request)
         all_valid = True
         if not form.is_valid():
             all_valid = False
@@ -72,19 +104,6 @@ class EditView(CreateView, SingleObjectMixin):
             messages.warning(request, _('The data could not be saved due to errors.'))
             return self.render_to_response(context)
         else:
-            saved_something = False
-            if form.has_changed():
-                form.save()
-                saved_something = True
-                
-            for formset in self.formsets.values():
-                for subform in formset.forms:
-                    if subform.has_changed():
-                        subform.save()
-                        saved_something = True
-            if saved_something:
-                messages.success(request, _('The data was updated.'))
-            else:
-                messages.info(request, _('The data contained not changes and therefore was not updated.'))
+            self.save_post_hook(request, form)
                 
         return redirect(self.raiadmin.default_action(self.raiadmin).get_url_name())
