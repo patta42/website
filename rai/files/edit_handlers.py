5722baa943de16c9f571bcc0759cf1dea5fd37a6
patta42 <patrick.happel@rub.de>
2020-05-20 08:20:16 +0200

    working(?) commit

--- rai/files/edit_handlers.py
+++ rai/files/edit_handlers.py
@@ -1,3 +1,6 @@
+from .models import RAICollection
+from .internals import REGISTERED_COLLECTIONS
+
 from pprint import pprint
 
 from django.template.loader import render_to_string
@@ -6,8 +9,11 @@ from django.utils.safestring import mark_safe
 from rai.edit_handlers import RAIInlinePanel, RAIFieldPanel, RAICollectionPanel, RAIMultiFieldPanel
 from rai.widgets import RAIFileInput
 
+
 class FileListPanel(RAIInlinePanel):
     template = 'rai/files/edit_handlers/file-list-panel.html'
+    children = []
+    
     def __init__(self, *args, **kwargs):
         self.panels = [
             FileFieldPanel('doc'),
@@ -17,30 +23,58 @@ class FileListPanel(RAIInlinePanel):
         
         super().__init__(*args, **kwargs)
 
+    def required_formsets(self):
+        return {}
+        
     def clone_kwargs(self):
         kwargs = super().clone_kwargs()
         kwargs['collection'] = self.collection
         return kwargs
+
+    def on_form_bound(self):
+        if not hasattr(self, 'children'):
+            self.children= []
+            
+    def render(self):
+        return render_to_string(self.template, {
+            'self' : self
+        })
+                                
     
     def on_request_bound(self):
         super().on_request_bound()
         add_file_panel = AddFileFieldPanel('doc')
         add_file_panel = add_file_panel.bind_to(model = self.model, request = self.request)
-        self.children.append(add_file_panel)
+        if not self.children:
+            self.children.append(add_file_panel)
         
     def on_instance_bound(self):
         children = []
-        for child in self.children:
+        self.db_field = self.instance._meta.get_field(self.relation_name)
+        objects = self.db_field.related_model.objects.filter(decorated_model = self.instance)
+        child_edit_handler = self.get_child_edit_handler()
+        form_class = child_edit_handler.get_form_class()
+        form = form_class()
+        
+        for obj in objects:
+            children.append(
+                child_edit_handler.bind_to(model = self.db_field.related_model, form = form, instance=obj)
+            )
+
+        new_children = []
+        for child in children + self.children:
             if isinstance(child, AddFileFieldPanel):
-                children.append(
+                new_children.append(
                     child.bind_to(
                         model = self.model,
                         request=self.request,
                         instance=self.instance)
                 )
             else:
-                children.append(child)
-        self.children = children
+                new_children.append(child)
+        self.children = new_children
+
+    
         
 class FileFieldPanel(RAIFieldPanel):
     # object_template = 'rai/files/edit_handlers/file-field-panel_as-object.html'
@@ -61,6 +95,9 @@ class AddFileFieldPanel(RAIFieldPanel):
             return [self.classname]
         return []
 
+    def render_form_content(self):
+        return self.render_as_field()
+    
     def render_as_field(self):
         return mark_safe(render_to_string(self.field_template, {
             'instance' : self.instance,
