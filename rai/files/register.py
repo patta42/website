cd94a249cd1926d01033becd5077f9f4d023772d
patta42 <patrick.happel@rub.de>
2020-05-22 07:27:38 +0200

    initial commit

---
+++ rai/files/register.py
@@ -0,0 +1,19 @@
+from .models import RAICollection
+
+from .base import RAIDocumentCollection, register_collection
+from .views import document_edit, add_document, delete_document
+
+from django.urls import path
+
+from wagtail.core import hooks
+
+register_collection(RAIDocumentCollection)
+
+
+@hooks.register('register_rai_url')
+def rai_file_urls():
+    return [
+        path('rai/files/<pk>/edit/', document_edit, name = 'rai_file_edit'),
+        path('rai/files/<pk>/delete/', delete_document, name = 'rai_file_delete'),
+        path('rai/files/<rai_collection>/<collection_pk>/<obj_pk>/add', add_document, name = 'rai_files_add')
+    ]
