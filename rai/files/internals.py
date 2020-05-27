893e606a1ce739c41a634ccb2dfc30e700490595
patta42 <patrick.happel@rub.de>
2020-05-27 17:05:00 +0200

    removed debug output

--- rai/files/internals.py
+++ rai/files/internals.py
@@ -4,9 +4,5 @@ REGISTERED_COLLECTIONS = {}
 
 for fn in hooks.get_hooks('rai_document_collection'):
     collection = fn()
-    print('Collections before:')
-    print(REGISTERED_COLLECTIONS)
     REGISTERED_COLLECTIONS.update(collection)
-    print('Collections after:')
-    print(REGISTERED_COLLECTIONS)
     
