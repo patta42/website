c62d1321b7b723deb4c81433c83d161e6baf1e42
patta42 <patrick.happel@rub.de>
2020-05-28 22:50:51 +0200

    Added abstract ModelWithJsonField and ListViewSettings

--- rai/settings/models.py
+++ rai/settings/models.py
@@ -1,5 +1,5 @@
 from .fields import JsonField
-from django.conf import settings
+from django.conf import settings as dj_settings
 from django.db import models
 
 import json
@@ -7,10 +7,33 @@ import json
 class ListFilterSettings(models.Model):
     view_name = models.CharField(max_length = 128)
     filter_spec = models.CharField(max_length = 512)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
+    user = models.ForeignKey(dj_settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
 
-class AdminMenuSettings(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
+
+class ModelWithJsonField(models.Model):
+    class Meta:
+        abstract = True
+        
+    def save(self, *args, **kwargs):
+        # this jsonifies every JsonField
+        for field in self._meta.fields:
+            if isinstance(field, JsonField):
+                attr = getattr(self, field.name)
+                if attr and not isinstance(attr, str):
+                    setattr(self, field.name, json.dumps(attr))
+                    
+        super().save(*args, **kwargs)
+
+class ListViewSettings(ModelWithJsonField):
+    view_name = models.CharField(max_length = 128)
+    settings =   JsonField(
+        blank = True
+    )
+    user = models.ForeignKey(dj_settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
+
+    
+class AdminMenuSettings(ModelWithJsonField):
+    user = models.ForeignKey(dj_settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
     group_order = JsonField(
         blank = True
     )
@@ -21,15 +44,15 @@ class AdminMenuSettings(models.Model):
         blank = True
     )
 
-    def save(self, *args, **kwargs):
-        # this jsonifies every JsonField
-        for field in self._meta.fields:
-            if isinstance(field, JsonField):
-                attr = getattr(self, field.name)
-                if attr and not isinstance(attr, str):
-                    setattr(self, field.name, json.dumps(attr))
+    # def save(self, *args, **kwargs):
+    #     # this jsonifies every JsonField
+    #     for field in self._meta.fields:
+    #         if isinstance(field, JsonField):
+    #             attr = getattr(self, field.name)
+    #             if attr and not isinstance(attr, str):
+    #                 setattr(self, field.name, json.dumps(attr))
                     
-        super().save(*args, **kwargs)
+    #     super().save(*args, **kwargs)
 
     def as_dict(self):
         dct = {
