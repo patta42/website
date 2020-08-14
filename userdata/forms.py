2d1c9147629f7a094fccdcad85c10433168e7557
patta42 <patrick.happel@rub.de>
2020-08-14 10:20:59 +0200

    added QS for users and staff

--- userdata/forms.py
+++ userdata/forms.py
@@ -1,8 +1,13 @@
+import datetime
+
 from django.forms.models import ModelForm
 from django import forms
+from django.contrib.contenttypes.models import ContentType
+from django.db.models import Q
 from django.utils.translation import ugettext_lazy as _
 
 from wagtail.admin import widgets
+from wagtail.core.models import Page
 from wagtail.utils.widgets import WidgetWithScript
 
 from userinput.models import RUBIONUser
@@ -82,3 +87,28 @@ class ManySafetyInstructionsForm( forms.Form ):
 
         self.fields['users'].choices = ( (user.id, '{}, {}'.format(user.name, user.first_name) ) for user in rusers )
         self.fields['staff'].choices = ( (user.id, '{}, {}'.format(user.last_name, user.first_name) ) for user in rstaff )
+
+
+def user_and_staff_qs():
+    exclude = []
+    for staff in StaffUser.objects.filter(user__isnull = False):
+        if RUBIONUser.objects.filter(linked_user = staff.user).exists():
+            exclude.append(staff.pk)
+    qs = (
+        Page.objects.
+        filter(content_type__in = [
+            ContentType.objects.get_for_model(StaffUser),
+            ContentType.objects.get_for_model(RUBIONUser)]
+        ).
+        exclude(pk__in = exclude).
+        filter(Q(expire_at__isnull = True)|Q(expire_at__gte = datetime.datetime.now())).
+        order_by('title')
+    )
+    return qs
+        
+class UserAndStaffChoiceField(forms.ChoiceField):
+    def __init__(self, *args, **kwargs):
+        choices = [('', '--')]
+        for p in user_and_staff_qs():
+            choices.append((p.pk, p.title))
+        super().__init__(choices = choices, *args, **kwargs)
