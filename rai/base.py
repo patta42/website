475f3d85372994510685d83f23ac198f22f88d66
patta42 <patrick.happel@rub.de>
2020-08-14 09:22:54 +0200

    added show()-method

--- rai/base.py
+++ rai/base.py
@@ -45,11 +45,15 @@ class RAIAdmin (RAIRegisterable):
         for Action in self.item_actions:
             action = Action(self)
             urls = urls + action.get_url_for_registration()
-
         return urls
+
     def get_default_url(self):
         action = self.default_action(self)
         return action.get_href()
+
+    def show(self, request=None):
+        return self.default_action(self).show(request)
+        
     
 class RAIModelAdmin (RAIAdmin):
     model = None
@@ -90,6 +94,59 @@ class RAIAdminGroup(RAIRegisterable):
             urls = urls + comp.get_url_for_registration()
         return urls
 
+class RAIDecoration:
+    """
+    A relation represents a DB relation via a relation model with (ususally) two 
+    ForeignKeys. One points to a RAI models such as comment or file, the second one 
+    to the model that is "decorated" with this.
+
+    The decorated model should be defined as `decorated_model` in the relation model, the 
+    RAI model as `rai_model`, which should be usually set in an abstract class by the RAI
+    app. 
+    """
+    def __init__(self, decoration):
+        self.decoration = decoration
+
+    def _id_from_opts(self, model):
+        opts = model._meta
+        return '{}.{}'.format(opts.app_label, opts.model_name)
+    @property
+    def decorated_model_id(self):
+        return self._id_from_opts(self.get_decorated_model())
+
+    @property
+    def rai_model_id(self):
+        return self._id_from_opts(self.get_rai_model())
+
+    @property
+    def field_in_decorated_model(self):
+        return self.decoration.decorated_model.field.remote_field.related_name
+        
+
+    def get_rai_model(self):
+        return self.decoration.rai_model.field.related_model().__class__
+
+    def get_decorated_model(self):
+        return self.decoration.decorated_model.field.related_model().__class__
+
+    def get_queryset(self):
+        pass
+    
+    def register(self):
+        @hooks.register('rai_decoration')
+        def get_decoration():
+            decoration = { self.decorated_model_id : self }
+            return (self.rai_model_id, decoration)
+            
+
+        
+def rai_register_decoration(decoration):
+    decorator = RAIDecoration(decoration)
+    decorator.register()
+
+def rai_register_decorations(decorations):
+    for decoration in decorations:
+        rai_register_decoration(decoration)
     
 def rai_register(rai_class):
     """
@@ -102,3 +159,13 @@ def rai_register(rai_class):
 
 
 
+def raiify(**kwargs):
+    def wrapper(fnc):
+        context_description = kwargs.get('context_description', None)
+        admin_help_text = kwargs.get('admin_help_text', None)
+        if context_description:
+            fnc.context_description = context_description
+        if admin_help_text:
+            fnc.admin_help_text = admin_help_text
+        return fnc
+    return wrapper
