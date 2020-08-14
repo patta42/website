3dc935a62afa2a8fccfa0978d55f711333dacc99
patta42 <patrick.happel@rub.de>
2020-08-14 09:41:40 +0200

    added registering ondemand documents

--- rai/files/internals.py
+++ rai/files/internals.py
@@ -5,4 +5,16 @@ REGISTERED_COLLECTIONS = {}
 for fn in hooks.get_hooks('rai_document_collection'):
     collection = fn()
     REGISTERED_COLLECTIONS.update(collection)
+
+REGISTERED_ONDEMAND_DOCUMENTS = {}
+
+for fn in hooks.get_hooks('rai_document_on_demand'):
+    key, doc = fn()
+    relation = doc.relation.__name__
+
+    docs = REGISTERED_ONDEMAND_DOCUMENTS.get(relation, {})
+    docs.update({key:doc})
+    REGISTERED_ONDEMAND_DOCUMENTS.update({relation: docs})
+    
+    
     
