3ae27ee44fb4d57ea2599cd91a90833471175a7d
patta42 <patrick.happel@rub.de>
2020-08-14 10:13:20 +0200

    import all register.py files

--- rai/wagtail_hooks.py
+++ rai/wagtail_hooks.py
@@ -1,8 +1,16 @@
 from rai.base import rai_register
+from rai.notifications.register import RAINotificationsGroup
 from rai.permissions.register import RAIPermissionsGroup
 
+import rai.comments.register
+import rai.files.register
+import rai.help.register
+import rai.mail.register
 import rai.markdown.register
+import rai.panels.register
+import rai.settings.register
 
+rai_register(RAINotificationsGroup)
 rai_register(RAIPermissionsGroup)
 
 
