cd1fef1e55b0c35eab313b403276293a201bb913
patta42 <patrick.happel@rub.de>
2020-08-14 09:38:24 +0200

    added EHs

--- rai/files/edit_handlers.py
+++ rai/files/edit_handlers.py
@@ -1,5 +1,5 @@
 from .models import RAICollection
-from .internals import REGISTERED_COLLECTIONS
+
 
 from pprint import pprint
 
@@ -7,6 +7,7 @@ from django.template.loader import render_to_string
 from django.utils.safestring import mark_safe
 
 from rai.edit_handlers import RAIInlinePanel, RAIFieldPanel, RAICollectionPanel, RAIMultiFieldPanel
+from rai.edit_handlers.generic import RAIEditHandler 
 from rai.widgets import RAIFileInput
 
 
@@ -58,7 +59,7 @@ class FileListPanel(RAIInlinePanel):
         
         for obj in objects:
             children.append(
-                child_edit_handler.bind_to(model = self.db_field.related_model, form = form, instance=obj)
+                child_edit_handler.bind_to(model = self.db_field.related_model, form = form, instance=obj, request = self.request)
             )
 
         new_children = []
@@ -83,7 +84,8 @@ class FileFieldPanel(RAIFieldPanel):
     def render_as_field(self):
         return mark_safe(render_to_string(self.field_template, {
             'instance' : self.instance,
-            'classes' : self.classes()
+            'classes' : self.classes(),
+            'may_delete' : self.request.user == self.instance.doc.uploaded_by_user or self.request.user.is_superuser 
         }))
 
     
@@ -105,3 +107,79 @@ class AddFileFieldPanel(RAIFieldPanel):
             'model' : self.model,
             'self' : self
         }))
+
+
+class OnDemandFileListPanel(RAIEditHandler):
+    template = 'rai/files/edit_handlers/on-demand-file-list-panel.html'
+    is_collapsable = True
+    children = []
+    
+    def __init__(self, relation_name, heading = '', *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.relation_name = relation_name
+        self.heading = heading
+        
+    def clone_kwargs(self):
+        kwargs = super().clone_kwargs()
+        kwargs.update(
+            relation_name=self.relation_name
+        )
+        return kwargs
+
+    def on_model_bound(self):
+        from .internals import REGISTERED_ONDEMAND_DOCUMENTS
+        self.relation = getattr(self.model, self.relation_name)
+        self.relation_model = self.relation.rel.related_model
+        self.registered_documents = REGISTERED_ONDEMAND_DOCUMENTS[self.relation_model.__name__]
+    def on_instance_bound(self):
+        query = self.relation_model.objects.filter(decorated_model = self.instance)
+        children = []
+        
+        for key, value in self.registered_documents.items():
+
+            eh = OnDemandFilePanel()
+            eh = eh.bind_to(
+                model = self.relation_model,
+                instance = self.instance,
+                document = value
+            ) 
+            children.append(eh)
+        self.children = children
+        
+    def render(self):
+        return render_to_string(self.template, {
+            'self': self,
+        })
+
+
+class OnDemandFilePanel(RAIEditHandler):
+    template = 'rai/files/edit_handlers/on-demand-file-panel.html'
+    def __init__(self, *args, **kwargs):
+        self.document = None
+        
+        super().__init__(*args, **kwargs)
+        
+    def bind_to(self, model = None, instance = None, request = None, document = None):
+        new = super().bind_to(model = model, instance = instance, request = request)
+        new.document = self.document if document is None else document
+
+        if new.document is not None:
+            new.on_document_bound()
+
+        return new
+
+    def on_document_bound(self):
+        request = self.model.objects.filter(decorated_model = self.instance, key = self.document.identifier)
+        self.exists = request.exists()
+        if self.exists:
+            obj = request[0]
+            self.RAIdocument = obj.doc
+        
+
+    def render(self):
+        from .internals import REGISTERED_ONDEMAND_DOCUMENTS
+        registered_documents = REGISTERED_ONDEMAND_DOCUMENTS[self.model.__name__]
+        
+        self.on_demand_document = registered_documents.get(self.document.identifier, None)
+        return render_to_string(self.template, {'self' : self })
+        
