f5338446f80cc075b1a022d21494db86936bf526
patta42 <patrick.happel@rub.de>
2020-08-14 09:24:18 +0200

    added import of MultiFormCreateView

--- rai/default_views/__init__.py
+++ rai/default_views/__init__.py
@@ -5,3 +5,5 @@ from .delete import DeleteView
 from .inactivate import InactivateView
 from .history import HistoryView
 from .create import CreateView
+from .multiform_create import MultiFormCreateView
+
