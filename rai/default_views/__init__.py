a5c8b3479eefb9940321cd26f471e5eb3cad7bbc
patta42 <patrick.happel@rub.de>
2020-10-23 08:09:05 +0200

    added ActivateView

--- rai/default_views/__init__.py
+++ rai/default_views/__init__.py
@@ -3,6 +3,7 @@ from .detail import DetailView
 from .edit import EditView
 from .delete import DeleteView
 from .inactivate import InactivateView
+from .activate import ActivateView
 from .history import HistoryView
 from .create import CreateView
 from .multiform_create import MultiFormCreateView
