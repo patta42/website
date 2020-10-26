4a3620e019ee5eb36e6da1b369d8fef2a550daeb
patta42 <patrick.happel@rub.de>
2020-10-26 08:58:27 +0100

    big fix

--- userinput/templatetags/userinput_rai_tags.py
+++ userinput/templatetags/userinput_rai_tags.py
@@ -1,9 +1,9 @@
 import datetime
 
-
 from django import template
 from django.template.loader import render_to_string
 
+from userdata.models import SafetyInstructionUserRelation
 from userinput.models import RUBIONUser
 
 register = template.Library()
@@ -20,3 +20,34 @@ def workgroup_render_members(wg):
             'inactive_users' : inactive_users
         }
     )
+
+@register.simple_tag
+def get_last_instruction( ruser, instruction ):
+    qs = ruser.rubion_user_si.filter(instruction = instruction).order_by('-date') 
+    if qs.count() > 0:
+        return qs[0]
+    return None
+
+@register.simple_tag
+def get_last_instruction_percentage(t, instruction):
+    valid_for = instruction.is_valid_for * 365
+    today = datetime.date.today()
+    delta = (today - t).days
+    rel = min(1,float(delta)/float(valid_for))
+    return int(rel*100)
+
+@register.simple_tag
+def get_last_instruction_indicator_css(t, instruction):
+    valid_for = instruction.is_valid_for * 365
+    today = datetime.date.today()
+    delta = (today - t).days
+    rel = min(1,float(delta)/float(valid_for))
+    if rel < .75:
+        bgcolor = '00c000'
+    elif rel < .9:
+        bgcolor = 'c0c000'
+    else:
+        bgcolor = 'c00000'
+        
+
+    return 'background-color:#{};width:{}%'.format(bgcolor, rel*100)
