8d26d63b2009a3dbf9e99b1f58c63728f57d01a5
patta42 <patrick.happel@rub.de>
2020-11-26 09:21:41 +0100

    added some tags for an overview panel

--- userdata/templatetags/userdata_staff_tags.py
+++ userdata/templatetags/userdata_staff_tags.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django import template
 register = template.Library()
 
@@ -8,3 +10,17 @@ def render_connections( user ):
     return {
         'user' : user
     }
+
+@register.simple_tag
+def get_last_instruction( staff, instruction ):
+    qs = staff.staff_user_si.filter(instruction = instruction).order_by('-date') 
+    if qs.count() > 0:
+        return qs[0]
+    return None
+@register.simple_tag
+def get_last_instruction_percentage(t, instruction):
+    valid_for = instruction.is_valid_for * 365
+    today = datetime.date.today()
+    delta = (today - t).days
+    rel = min(1,float(delta)/float(valid_for))
+    return int(rel*100)
