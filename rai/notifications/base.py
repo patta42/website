2a054f36545103902752a9062120859efc40b1cd
patta42 <patrick.happel@rub.de>
2021-01-16 08:56:55 +0100

    Changed language in template for rendering

--- rai/notifications/base.py
+++ rai/notifications/base.py
@@ -5,7 +5,10 @@ from .models import NotificationTemplate
 from django.core.exceptions import ImproperlyConfigured
 from django.core.mail import EmailMessage
 from django.conf import settings as DJANGO_SETTINGS 
+
 from django.db.models.signals import pre_save, post_save
+from django.dispatch import receiver, Signal
+
 from django.template import Template, Context
 from django.template.loader import get_template
 
@@ -16,135 +19,51 @@ from userinput.signals import (
     post_page_move, pre_page_move
 )
 
+
+
 from wagtail.core import hooks
-from wagtail.core.models import Page 
+from wagtail.core.models import Page, PageRevision
 from wagtail.core.signals import (
     page_published, page_unpublished,
 )
 
 from website.models import SentMail
 
-class RAIListener:
-    model = None
-    identifier = None
-    signal = None
-    needs_old_instance = True
-    old_instance = None
 
-    
-    def register(self):
-        self.register_signal()
-        
-    def register_signal(self, weak = False):
-        """
-        Registers the signal
-        """
-        signal = None
-        if self.signal == 'pre_save':
-            signal = pre_save
-        if self.signal == 'post_save':
-            signal = post_save
-        if self.signal == 'page_published':
-            signal = page_published
-        if self.signal == 'page_unpublished':
-            signal = page_unpublished
-        if self.signal == 'pre_page_move':
-            signal = pre_page_move
-        if self.signal == 'post_page_move':
-            signal = post_page_move
-            
-        if signal:
-            signal.connect(
-                self.signal_received, sender=self.model,
-                dispatch_uid = self.identifier,
-                weak = weak
-            )
 
-    def signal_received(self, **kwargs):
-        self.new_instance = kwargs['instance']
-        self.signal_kwargs = kwargs 
-        if issubclass(self.new_instance.__class__, Page):
-            self.new_instance = self.new_instance.specific
-        if self.needs_old_instance:
-            if self.signal == 'page_published':
-                revisions = self.new_instance.revisions.order_by('-created_at')
-                self.old_instance = revisions[1].as_page_object()
-                self.changing_user = revisions[0].user
-            elif self.signal in  ['pre_page_move', 'post_page_move']:
-                self.old_workgroup = kwargs['parent_page_before'].get_parent().specific
-                self.new_workgroup = kwargs['parent_page_after'].get_parent().specific
-            else:
-                self.old_instance = self.model.objects.get(pk = self.new_instance.pk)
-            if self.signal == 'post_page_move':
-                revisions = self.new_instance.revisions.order_by('-created_at')
-                self.changing_user = revisions[0].user
-        if self.trigger_check():
-            self.process()
-
-    def trigger_check(self):
-        """
-        trigger_check is called to decide whether the listener should
-        execute self.process()
-
-        this method reads the corresponding setting, checks if django runs in debug mode and
-        returns True or False, accordingly. Subclasses should call this and implememnt their 
-        own logic.
-        """
-        if DJANGO_SETTINGS.DEBUG:
-            return True
-        
-        from .register import UseRAINotifications
-        setting =  UseRAINotifications()
-        return setting.value 
-        
-        
-    def process(self):
-        # subclasses should implement this
-        pass
-
-class RAIFieldChangedListenerMixin:
-    fields = []
-
-    def trigger_check(self):
-        if super().trigger_check():
-            for field in self.fields:
-                old = getattr(self.old_instance, field)
-                new = getattr(self.new_instance, field)
-                if  old != new: 
-                    return True
-        return False
-                
 class NotificationTemplateMixin:
     template = None
     template_en = None
     template_name = None
     template_name_en = None
-        
-    context_definition = {}
+    identifier = None
     
-    def register_template(self):
+    context_definition = {}
+
+    @classmethod
+    def register_template(Kls):
         """
         Adds the notification template to the database to allow
         editing.
         """
         
         try:
-            NotificationTemplate.objects.get(notification_id = self.identifier)
+            NotificationTemplate.objects.get(notification_id = Kls.identifier)
         except NotificationTemplate.DoesNotExist:
-            if not self.template_name:
-                template_string = self.template
+            if not Kls.template_name:
+                template_string = Kls.template
             else:
-                template = get_template(self.template_name)
+                template = get_template(Kls.template_name)
                 template_string = template.template.source
-            if not self.template_name_en:
-                template_string_en = self.template_en or ""
+            if not Kls.template_name_en:
+                template_string_en = Kls.template_en or ""
             else:
-                template_en = get_template(self.template_name_en)
+                template_en = get_template(Kls.template_name_en)
                 template_string_en = template.template.source
 
             
             nt = NotificationTemplate(
-                notification_id = self.identifier,
+                notification_id = Kls.identifier,
                 template = template_string,
                 template_en = template_string_en
             )
@@ -153,9 +72,11 @@ class NotificationTemplateMixin:
     def get_template(self, lang = 'de'):
         nt = NotificationTemplate.objects.get(notification_id = self.identifier)
         if lang == 'de':
-            return nt.template
+            tpl = nt.template
         if lang == 'en':
-            return nt.template_en
+            tpl = nt.template_en
+        return '{{% language \'{lang}\' %}}{tpl}{{% endlanguage %}}'.format(lang=lang, tpl = tpl)
+            
     def get_subject(self, lang = 'de'):
         nt = NotificationTemplate.objects.get(notification_id = self.identifier)
         if lang == 'de':
@@ -182,7 +103,7 @@ class NotificationTemplateMixin:
         return query
 
     def render_template(self, template, **kwargs):
-        tags2load = []
+        tags2load = ['i18n']
         context = {}
         for key, definition in self.context_definition.items():
             tags2load.append(getmodulename(getfile(definition['tags'])))
@@ -193,14 +114,17 @@ class NotificationTemplateMixin:
         tpl = Template(template)
         return tpl.render(Context(context))
         
-    def render_preview(self, template, **kwargs):
+    def render_preview(self, template, lang = 'de', **kwargs):
         render_kwargs = {}
         for key, definition in self.context_definition.items():
             pk = kwargs.get(definition['prefix'], None)
             if pk:
                 query = self._get_query_from_definition(definition)
                 render_kwargs.update({definition['prefix'] : query.get(pk = pk) })
-        return self.render_template(template, **render_kwargs)
+        return self.render_template(
+            '{{% language \'{lang}\' %}}{tpl}{{% endlanguage %}}'.format(tpl = template, lang = lang),
+            **render_kwargs
+        )
         
     def get_template_tags(self):
         context_tags = {}
@@ -240,6 +164,11 @@ class NotificationTemplateMixin:
         })
 
     def process(self):
+        from rai.settings.internals import get_rai_setting
+        use_rai_noties = get_rai_setting('rai.notifications.active')
+        if use_rai_noties:
+            use_rai_noties = use_rai_noties()
+            
         for mailinfo in self.mails_to_send:
             mail = EmailMessage(
                 mailinfo['subject'],
@@ -247,32 +176,192 @@ class NotificationTemplateMixin:
                 DJANGO_SETTINGS.RUBION_MAIL_FROM,
                 mailinfo['receivers']
             )
-                
-            mail.send()
+            if DJANGO_SETTINGS.DEBUG or use_rai_noties.value == True:
+                mail.send()
             SentMail.from_email_message(mail).save()
     
     
-class RAINotification(NotificationTemplateMixin, RAIListener):
+class RAINotification(NotificationTemplateMixin):
     title = None
     label = None
     help_text = None
-    
 
     internal = False
-    
-    def register(self):
-        super().register()
-        self.register_template()
-        self.register_with_wagtail()
 
-
-    def register_with_wagtail(self):
+    def __init__(self, **kwargs):
+        for key, val in kwargs.items():
+            setattr(self, key, val)
+        self.kwargs_list = kwargs.keys()
+    @classmethod
+    def register(Kls, Event = None):
+    #    super().register()
+        Kls.register_template()
+        Kls.register_with_wagtail()
+        if Event:
+            Event.add_callback(Kls.on_event)
+        
+    @classmethod
+    def register_with_wagtail(Kls):
         @hooks.register('rai_notification')
         def register_notification():
-            return self
+            return Kls
+        
+    @classmethod
+    def on_event(Kls, **kwargs):
+        # initiate object
+        print('\n\nEvent happend.\n\n')
+        print('kwargs are')
+        print(kwargs)
+        instance = Kls(**kwargs)
+        instance.prepare(**kwargs)
+        instance.process()
+        
+class RAIEvent:
+    '''
+    An event is a change in the data, like a new project or a modified user, or an 
+    expiring safety instruction or similar. Notifications are bound to an Event.
 
+    Events are similar to Signals, but more detailed. 
+    '''
     
+    # the model which undergoes some changes
+    model = None
 
+    # an internal verbose name for the related model
+    _verbose_name = None
+    
+    # the django signal that is emitted, if any
+    signal = None
+
+    # an event has an id
+    identifier = None
+
+    # an Event might have a description for explanation
+    description = None
+
+    # and a human-readable title
+    title = None
+
+    # the class-variable store for callbacks
+    _callbacks = {}
+    
+    @property
+    def verbose_name(self):
+        return self._verbose_name or self.model._meta.verbose_name
+    
+    @classmethod
+    def register(Kls):
+        # if an Event is connected to a signal, register `signal_received` as receiver function
+        # otherwise, do nothing special.
+        if Kls.signal:
+            @receiver(Kls.signal, sender = Kls.model, weak = False, dispatch_uid = Kls.identifier)
+            def receiver_fnc(sender, **kwargs):
+                instance = Kls()
+                instance.signal_received(sender, **kwargs)
+                
+    @classmethod
+    def add_callback(Kls, func, uid = None, **kwargs):
+        '''
+        adds the callback `func` to the event. To prevent multiple registration, set `uid` to 
+        some unique string. Additional arguments beside the ones from the Event passed to the 
+        callback should be given as kwargs
+        '''
+        KlsId = Kls.identifier
+        callbacks = Kls._callbacks.get(KlsId, [])
+        # prevent multiple registration 
+        for cb in callbacks:
+            if cb.uid and cb.uid == uid:
+                return
+        callbacks.append({
+            'uid' : uid,
+            'func' : func,
+            'kwargs' : kwargs
+        })
+        Kls._callbacks[KlsId] = callbacks
+
+        
+    def signal_received(self, sender, **kwargs):
+        # since Signals are more general than Events, an Event may not occur if a Signal is emitted
+        # In general, this method calls the the `check` method (which has to be defined by each Event)
+        # and if that returns True, emits the Event
+        if self.check(**kwargs):
+            self.emit(**kwargs)
+
+                
+    def get_emit_kwargs(self, **kwargs):
+        # a method which processes the kwargs received by emit (which are usually the ones received
+        # by `signal_received`) and generates the kwargs used by the Event.
+        return kwargs
+    
+    def emit(self, **kwargs):
+        # emits the Event. This can be called by `signal_received`, but also semi-manually for
+        # example by a cron job or similar. The kwargs usually provide additional information
+        # and are processed by `get_emit_kwargs` to generate the kwargs send by the Event
+        event_kwargs = self.get_emit_kwargs(**kwargs)
+        for cb in self.__class__._callbacks.get(self.identifier, []):
+            cb['func'](**{ **cb['kwargs'], **event_kwargs})
+        
+
+
+
+class PageContentChangedEvent(RAIEvent):
+    ''' 
+    An event that listens to the page_published signal.
+
+    It emits `old_instance`, `new_instance`, `user` and `changed_fields`
+    '''
+    signal = page_published
+
+    # cache emit kwargs is a flag which indicates whether the kwargs are stored in self._emit_kwargs
+    # once get_emit_kwargs has been called. Particularly useful to check the `changed_fields` kwarg
+    # without doing the logic twice
+
+    cache_emit_kwargs = False
+
+    def check(self, **kwargs):
+        # To distinguish between update and create, check if there is a previous revision
+        try:
+            kwargs['revision'].get_previous()
+            return True
+        except PageRevision.DoesNotExist:
+            return False
+        
+    def get_emit_kwargs(self, **kwargs):
+        cache = getattr(self, 'cache_emit_kwargs', False)
+        if cache and hasattr(self, '_emit_kwargs'):
+            return self._emit_kwargs
+        
+        ekw = {
+            'new_instance' : kwargs['instance'],
+            'user' : kwargs['revision'].user,
+            'changed_fields' : []
+        }
+        prev = kwargs['revision'].get_previous() 
+        old = prev.page.specific_class.from_json(prev.content_json)
+            
+        ekw['old_instance'] = old
+        excluded_fields = [
+            'first_published_at', 'last_published_at', 'latest_revision_created_at',
+            'live_revision', 'page_ptr'
+        ]
+        
+        for field in self.model._meta.fields:
+            if field.name in excluded_fields:
+                continue
+            if getattr(ekw['old_instance'], field.name, None) != getattr(ekw['new_instance'], field.name, None):
+                ekw['changed_fields'].append(field.name)
+
+        if cache:
+            self._emit_kwargs = ekw
+        return ekw
+
+            
+        
+class ObjectMovedEvent(RAIEvent):
+    '''
+    An Event that listens to `post_page_move`
+    '''
+    signal = post_page_move
     
 def register_listener(Listener):
     listener = Listener()
@@ -281,3 +370,6 @@ def register_listener(Listener):
 def register_listeners(lst):
     for l in lst:
         register_listener(l)
+
+def register_notification(Notification, event = None):
+    Notification.register(event)
