7e1729bc313dcfbe5c72b4a81a6cd137f8aef85f
patta42 <patrick.happel@rub.de>
2021-01-19 08:28:12 +0100

    updated cronjobs for expiring projects/safety instructions

--- userinput/cron.py
+++ userinput/cron.py
@@ -2,7 +2,8 @@ import datetime
 
 from dateutil.relativedelta import relativedelta
 
-from django.db.models import Q
+from django.conf import settings
+from django.db.models import Q, Count, Max
 
 from django_cron import CronJobBase, Schedule
 
@@ -12,6 +13,11 @@ from notifications.models import ProjectExpiredNotifications
 
 from userinput.models import Project, RUBIONUser
 from userinput.notifications import ProjectExpiredMailNotification
+from userinput.rai.events import (
+    ProjectExpiresSoonEvent, ProjectIsExpiredEvent,
+    SafetyInstructionExpiresThisYearEvent, SafetyInstructionExpiresSoonEvent,
+    SafetyInstructionIsExpiredEvent, SafetyInstructionNeverGiven
+)
 
 from website.models import EMailText
 
@@ -107,3 +113,128 @@ class WarnProjects( CronJobBase ):
         
         logger.info('Sent mail for project {} to {}'.format(project.title_de, ", ".join(to))) 
         
+def _active_projects():
+    return Project.objects.filter(Project.active_filter())
+        
+class RAIWarnProjects( CronJobBase ):
+    # CronJob that triggers Events for expiring projects
+
+    RUN_EVERY_MINS = 24 * 60 # once a day
+
+    schedule = Schedule(run_every_mins = RUN_EVERY_MINS)
+
+    code = 'userinput.warn_projects'
+
+    def do(self):
+            
+        self.today = datetime.date.today()
+        self.in_one_month = self.today + relativedelta(months = +1)
+        # get projects that expire within the next month:
+        projects = _active_projects()
+        expiring_projects = projects.filter(expire_at__lt = self.in_one_month, expire_at__gt = self.today)
+        expired_projects = projects.filter(expire_at__lte = self.today)
+        
+
+        for project in expiring_projects:
+            event = ProjectExpiresSoonEvent()
+            event.emit(
+                project = project,
+                date = self.today
+            )
+        for project in expired_projects:
+            event = ProjectIsExpiredEvent()
+            event.emit(
+                project = project,
+                date = self.today
+            )
+        
+class RAIWarnSafetyInstructions(CronJobBase):
+    # run once a day
+    RUN_EVERY_MINS = 24 * 60 # once a day
+    schedule = Schedule(run_every_mins = RUN_EVERY_MINS)
+    code = 'userinput.warn_safety_instructions'
+    # SOME NOTES ON THE QUERYSETS
+    # get the last safety instruction dates from a single rubionuser `ru`:
+    # ru.rubion_user_si.filter(instruction__in = ru.safety_instructions.filter(as_required = False).values_list('instruction', flat = True)).values('instruction').annotate(last = Max('date'))
+    
+    def do(self):
+        # active rubion users which are not staff members
+        active_users = RUBIONUser.objects.annotate(
+            staffcount = Count('linked_user__staffuser')
+        ).filter(staffcount = 0, locked = False)
+
+        for user in active_users.prefetch_related('safety_instructions'):
+            # get last safety instructions
+            inner_q = user.safety_instructions.filter(
+                as_required = False
+            ).values_list('instruction', flat = True)
+
+            # get and re-arrange last instructions
+            last_instructions = {}
+            validities = {}
+            last_instructions_q = user.rubion_user_si.filter(
+                instruction__in = inner_q
+            ).values('instruction', 'instruction__is_valid_for').annotate(last = Max('date'))
+            for li in last_instructions_q:
+                last_instructions[li['instruction']] = li['last']
+                validities[li['instruction']] = li['instruction__is_valid_for']
+                
+            
+            # The following "deadlines" trigger the events:
+            # For instructions which are valid for more than one year:
+            #
+            # - if the instruction expires within the next year
+            # SafetyInstructionExpiresThisYear
+            thisyear = []
+
+            # - if the instruction expires within the next two months:
+            # SafetyInstructionExpiresSoon
+            soon = []
+
+            # - if it is expired:
+            # SafetyInstructionIsExpired
+            expired = []
+
+            # - if a SI was never given yet:
+            # SafetyInstructionNeverGiven
+            never = []
+            
+            today = datetime.date.today()
+            two_months = today + relativedelta(months = 2)
+            next_year = today + relativedelta(years = 1)
+
+            # loop through the instructions required for the user
+            for rel in user.safety_instructions.filter(as_required = False):
+                last_date = last_instructions.get(rel.instruction.pk, None)
+                if not last_date:
+                    never.append(rel.instruction)
+                    continue
+                # add validity 
+                last_date += relativedelta(years = validities[rel.instruction.pk])
+                
+                if last_date < today:
+                    expired.append(rel.instruction)
+                    continue
+                if last_date < two_months:
+                    soon.append(rel.instruction)
+                    continue
+                if last_date < next_year and validities[rel.instruction.pk] > 1:
+                    thisyear.append(rel.instruction)
+                    
+            # emit the events:
+            if never:
+                event = SafetyInstructionNeverGiven()
+                event.emit(user = user, instructions = never)
+                
+            if expired:
+                event = SafetyInstructionIsExpiredEvent()
+                event.emit(user = user, instructions = expired)
+                
+            if soon:
+                event = SafetyInstructionExpiresSoonEvent()
+                event.emit(user = user, instructions = soon)
+
+            if thisyear:
+                event = SafetyInstructionExpiresThisYearEvent()
+                event.emit(user = user, instructions = thisyear)
+                
